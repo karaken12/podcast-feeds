a3e4ceb46b5535373b3516a6a87bf86036795dab
Tom Potts <karaken12@gmail.com>
2015-03-27 11:16:42 +0000

    Make feeds generate into main folder.

--- _plugins/podcast-generator.rb
+++ _plugins/podcast-generator.rb
@@ -1,10 +1,10 @@
 module Jekyllpodcasts
   class PodcastFeed < Jekyll::Page
-    def initialize(site, base, dir, podcast)
+    def initialize(site, base, dir, name, podcast)
       @site = site
       @base = base
       @dir = dir
-      @name = 'feed.xml'
+      @name = name + '.xml'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'feed.xml')
@@ -14,17 +14,10 @@ module Jekyllpodcasts
 
   class Generator < Jekyll::Generator
     def generate(site)
-      dir = site.config['baseurl']
-#      site.data['podcasts'].each do |podcast_pair|
-#        name = podcast_pair[0]
-#        podcast = podcast_pair[1]
-#        puts name
-#        puts podcast
-#        puts podcast['baseurl']
-#        puts '---'
-#      end
+      # Set dir so we save at the top level.
+      dir = ''
       site.data['podcasts'].each do |name, podcast|
-        site.pages << PodcastFeed.new(site, site.source, File.join(dir, podcast['baseurl']), podcast)
+        site.pages << PodcastFeed.new(site, site.source, dir, name, podcast)
       end
     end
   end
--- _plugins/podcast-length.rb
+++ _plugins/podcast-length.rb
@@ -1,11 +1,10 @@
 module Jekyllpodcasts
   class PodcastLength < Jekyll::Generator
     def generate(site)
-      fsdir = '/home/podcasts/html'
-      dir = site.config['baseurl']
+      fsdir = '/usr/share/nginx/html/podcasts/files/'
       site.data['podcasts'].each do |name, podcast|
         podcast['entries'].each do |entry|
-          location = fsdir + dir + podcast['baseurl'] + entry['url']
+          location = File.join(fsdir, podcast['baseurl'], entry['url'])
           entry['length'] = File.size(location)
         end
       end
