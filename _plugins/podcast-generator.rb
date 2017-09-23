365394874013f00f926610d17a138ffd064d8023
Tom Potts <karaken12@gmail.com>
2017-09-23 21:39:52 +0100

    Add a human-readable page for each feed.

--- _plugins/podcast-generator.rb
+++ _plugins/podcast-generator.rb
@@ -48,12 +48,28 @@ module Jekyllpodcasts
     end
   end
 
+  class PodcastPage < Jekyll::Page
+    def initialize(site, base, dir, name, podcast)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = name + '.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'podcast.html')
+
+      self.data['podcast'] = podcast
+      self.data['short_name'] = name
+    end
+  end
+
   class Generator < Jekyll::Generator
     def generate(site)
       # Set dir so we save at the top level.
       dir = ''
       site.data['podcasts'].each do |name, podcast|
         site.pages << PodcastFeed.new(site, site.source, dir, name, podcast)
+        site.pages << PodcastPage.new(site, site.source, dir, name, podcast)
       end
     end
   end
